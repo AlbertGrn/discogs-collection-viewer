8add6ca2e6aa08803efd0575db01f1603aa9e0f0
AlbertGrn <albert.scherf@web.de>
2023-03-10 19:56:57 +0100

    Added autocomplete compatibility
    and comment out start animation.

--- js/index.js
+++ js/index.js
@@ -6,30 +6,35 @@ const login_button = document.getElementById('login-submit-button');
 
 const login_box_input = document.getElementsByClassName('login-box-input');
 
+const login_form = document.querySelector("#login-box form");
+
 
 var loginManager;
 
 async function init() {
-    loginManager = new LoginManager(login_username, login_token);
+    loginManager = new LoginManager();
 
     //change border of input boxes on focus
-    login_box_input[0].addEventListener('focusin', function() {
+    login_box_input[0].addEventListener('focusin', function () {
         login_box_input[0].style.border = '3px solid var(--main-accent-color)'
     })
 
-    login_box_input[0].addEventListener('focusout', function() {
+    login_box_input[0].addEventListener('focusout', function () {
         login_box_input[0].style.border = '3px solid var(--main-dark-color)'
     })
 
-    login_box_input[1].addEventListener('focusin', function() {
+    login_box_input[1].addEventListener('focusin', function () {
         login_box_input[1].style.border = '3px solid var(--main-accent-color)'
     })
 
-    login_box_input[1].addEventListener('focusout', function() {
+    login_box_input[1].addEventListener('focusout', function () {
         login_box_input[1].style.border = '3px solid var(--main-dark-color)'
     })
 
-    login_button.addEventListener('click', function() {
+
+    login_form.addEventListener('submit', (event) => {
+        event.preventDefault();
+    //login_button.addEventListener('click', function() {
         let username = cleanString(login_username.value);
         let password = cleanString(login_token.value);
         loginManager.addEventListener('onLoginFailed', loginFailCss)
